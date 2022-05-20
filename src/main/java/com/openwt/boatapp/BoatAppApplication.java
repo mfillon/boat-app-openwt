c7058e6d357c02680acc9c39e6f83ec92cb118ea
Matthieu Fillon <mfillon@gmail.com>
2022-05-20 23:45:43 +0200

    Fix indentation

--- src/main/java/com/openwt/boatapp/BoatAppApplication.java
+++ src/main/java/com/openwt/boatapp/BoatAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BoatAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoatAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BoatAppApplication.class, args);
+    }
 
 }
--- src/test/java/com/openwt/boatapp/BoatAppApplicationTests.java
+++ src/test/java/com/openwt/boatapp/BoatAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BoatAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
