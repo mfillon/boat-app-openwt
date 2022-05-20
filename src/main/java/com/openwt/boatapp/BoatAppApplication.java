d319d502b33a5d3330609fe99e343c6fb0be9f8b
Matthieu Fillon <mfillon@gmail.com>
2022-05-20 13:35:23 +0200

    Setup BE Spring Boot structure
    Closes #1

---
+++ src/main/java/com/openwt/boatapp/BoatAppApplication.java
@@ -0,0 +1,13 @@
+package com.openwt.boatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatAppApplication.class, args);
+	}
+
+}
