443a83d01d9c6e61b2b4a4de1cf78cb55ff1f8ef
Matthieu Fillon <mfillon@gmail.com>
2022-05-23 22:16:29 +0200

    Clean imports

--- src/main/java/com/openwt/boatapp/config/WebSecurityConfig.java
+++ src/main/java/com/openwt/boatapp/config/WebSecurityConfig.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
