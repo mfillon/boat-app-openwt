8136fa376bc2e2ee6797f7ecdd5903b000a58cea
Matthieu Fillon <mfillon@gmail.com>
2022-05-24 00:42:15 +0200

    Fix basic auth management on client side

--- frontend/src/services/basic-auth.interceptor.js
+++ frontend/src/services/basic-auth.interceptor.js
@@ -2,13 +2,14 @@ import axios from 'axios';
 
 import { userService } from '@/services/user.service.js';
 
-export function basicAuthInterceptor() {
+export default function basicAuthInterceptor() {
     axios.interceptors.request.use(request => {
         // add basic auth header if account is logged in and request is to the api url
         const user = userService.getCurrentUser();
         const isLoggedIn = user;
         const isApiUrl = request.url.startsWith('/api/');
         if (isLoggedIn && isApiUrl) {
+            console.log('Interceptor. adding basic auth data : ', user.authdata)
             request.headers.common.Authorization = `Baisc ${user.authdata}`;
         }
 
