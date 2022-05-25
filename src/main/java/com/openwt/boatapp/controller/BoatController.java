75993a7e75be03a3fec868ada0f176ccf54fd723
Matthieu Fillon <mfillon@gmail.com>
2022-05-25 10:50:10 +0200

    Add delete boat endpoint with validation
    - disable CSRF in Spring Security config to authorize DELETE/PUT/POST,
      not needed for stateless REST app
    - expose DELETE /boat/{boatId} endpoint returning 204 (No Content) if
      success
    - validation by checking boat owner before deletion
    - unit tests
    
    Closes #12

--- src/main/java/com/openwt/boatapp/controller/BoatController.java
+++ src/main/java/com/openwt/boatapp/controller/BoatController.java
@@ -1,14 +1,18 @@
 package com.openwt.boatapp.controller;
 
 import java.security.Principal;
+import java.util.Optional;
 
 import com.openwt.boatapp.model.Boat;
 import com.openwt.boatapp.repository.BoatRepository;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -33,5 +37,24 @@ public class BoatController {
                     "Requested owner is different than logged in user");
         }
         return repository.findByOwner(owner);
+    }
+
+    @DeleteMapping("/{boatId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBoat(@PathVariable long boatId, Principal principal) {
+        log.debug("Delete boat request for boat ID # {}", boatId);
+
+        Optional<Boat> boat = repository.findById(boatId);
+        if (boat.isEmpty()) {
+            log.debug("Boat with ID #{} not found", boatId);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        String owner = boat.get().getOwner();
+        if (!owner.equals(principal.getName())) {
+            log.warn("Boat owner is different than logged in user");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Boat owner is different than logged in user");
+        }
+        repository.deleteById(boatId);
     }
 }
