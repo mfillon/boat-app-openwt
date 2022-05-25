62c42d7653f06c1038172674d86c2c41afad944c
Matthieu Fillon <mfillon@gmail.com>
2022-05-25 10:19:12 +0200

    Check authenticated user is owner to retrieve list of boats

--- src/main/java/com/openwt/boatapp/controller/BoatController.java
+++ src/main/java/com/openwt/boatapp/controller/BoatController.java
@@ -1,17 +1,24 @@
 package com.openwt.boatapp.controller;
 
+import java.security.Principal;
+
 import com.openwt.boatapp.model.Boat;
 import com.openwt.boatapp.repository.BoatRepository;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/boats")
+@Slf4j
 public class BoatController {
-    
+
     private BoatRepository repository;
 
     public BoatController(BoatRepository repository) {
@@ -19,8 +26,12 @@ public class BoatController {
     }
 
     @GetMapping
-    public Iterable<Boat> getBoats(@RequestParam String owner) {
-        //TODO check if requested owner is authenticated user
+    public Iterable<Boat> getBoats(@RequestParam String owner, Principal principal) {
+        if (!owner.equals(principal.getName())) {
+            log.warn("Requested owner is different than logged in user");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Requested owner is different than logged in user");
+        }
         return repository.findByOwner(owner);
     }
 }
--- src/test/java/com/openwt/boatapp/controller/BoatControllerTest.java
+++ src/test/java/com/openwt/boatapp/controller/BoatControllerTest.java
@@ -32,22 +32,29 @@ public class BoatControllerTest {
 
     @BeforeEach
     public void setup() {
-        Boat boat1 = new Boat(1L,  "user1", "boat1");
-        Boat boat2 = new Boat(2L,  "user1", "boat2");
+        Boat boat1 = new Boat(1L, "user1", "boat1");
+        Boat boat2 = new Boat(2L, "user1", "boat2");
         List<Boat> boats = Arrays.asList(boat1, boat2);
         when(repo.findByOwner("user1")).thenReturn(boats);
     }
-
 
     @Test
     @WithMockUser("user1")
     public void test_get_boat_by_user() throws Exception {
         mvc.perform(get("/api/boats?owner=user1")
-            .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$", hasSize(2)))
-            .andExpect(jsonPath("$[0].name", is("boat1")))
-            .andExpect(jsonPath("$[1].name", is("boat2")));
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].name", is("boat1")))
+                .andExpect(jsonPath("$[1].name", is("boat2")));
+    }
+
+    @Test
+    @WithMockUser("user2")
+    public void shouldReturn400ForAuthenticatedUserDifferentOwner() throws Exception {
+        mvc.perform(get("/api/boats?owner=user1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
     }
 
 }
