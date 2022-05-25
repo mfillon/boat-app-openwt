59d72de76dc21a4b98b6c7d9afae8b0c22389b87
Matthieu Fillon <mfillon@gmail.com>
2022-05-25 18:01:53 +0200

    Display boat details in modal
    Closes #27

---
+++ frontend/src/components/BoatDetailsModal.vue
@@ -0,0 +1,71 @@
+<template>
+  <teleport to="body">
+      <dialog open>
+        <header>
+          <h2>{{name}}</h2>
+        </header>
+          <section>
+            <div>
+                <p>{{ description }}</p>
+            </div>
+          </section>
+          <menu>
+            <button type="button" @click="$emit('close')">Close</button>
+          </menu>
+      </dialog>
+  </teleport>
+</template>
+<script>
+
+export default {
+  props: ['name', 'description'],
+  emits: ['close'],
+};
+</script>
+<style scoped>
+/* div {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.75);
+  z-index: 10;
+} */
+
+dialog {
+  margin: 0;
+  padding: 0;
+  position: fixed;
+  top: 20vh;
+  left: 10%;
+  width: 80%;
+  background-color: white;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  border-radius: 12px;
+  border: none;
+}
+
+header {
+  background-color: purple;
+  border-radius: 12px 12px 0 0;
+  width: 100%;
+  padding: 1rem;
+}
+
+h2 {
+  margin: 0;
+  color: white;
+}
+
+section {
+    padding: 1rem;
+}
+
+menu {
+    display: flex;
+    justify-content: flex-end;
+    padding: 1rem;
+    margin: 0;
+}
+</style>
--- frontend/src/components/BoatItem.vue
+++ frontend/src/components/BoatItem.vue
@@ -1,6 +1,6 @@
 <template>
     <li>
-        {{ name }} ({{ description }})
+        <a href @click.prevent="showBoatDetails">{{ name }} ({{ description }})</a>
         <button @click="editBoat"><i class="fa fa-edit"></i></button>
         <button @click="deleteBoat"><i class="fa fa-trash"></i></button>
     </li>
@@ -12,10 +12,17 @@
         @close="closeEditModal"
         @boat-updated="$emit('boat-modified')"
     ></BoatActionModal>
+    <BoatDetailsModal v-if="displayBoatDetailsModal"
+        :name="name"
+        :description="description"
+        @close="closeBoatDetailsModal"
+    >
+    </BoatDetailsModal>
 </template>
 <script>
 import { boatService } from '@/services/boat.service'
 import BoatActionModal from '@/components/BoatActionModal.vue'
+import BoatDetailsModal from '@/components/BoatDetailsModal.vue'
 
 export default {
 
@@ -27,10 +34,11 @@ export default {
     emits: ['boat-modified'],
     data() {
         return {
-            openEditModal: false
+            openEditModal: false,
+            displayBoatDetailsModal: false
         }
     },
-    components: { BoatActionModal },
+    components: { BoatActionModal, BoatDetailsModal },
     methods: {
         deleteBoat() {
             if (confirm('Are you sure you want to delete this boat?')) {
@@ -45,6 +53,12 @@ export default {
         },
         closeEditModal() {
             this.openEditModal = false;
+        },
+        showBoatDetails() {
+            this.displayBoatDetailsModal = true;
+        },
+        closeBoatDetailsModal() {
+            this.displayBoatDetailsModal = false;
         }
     }
 }
